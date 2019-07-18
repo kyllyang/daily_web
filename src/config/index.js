393360c7d36fe7a58351f5882e6c4d9b6d3bd250
kyllyang <kyllyang@gmail.com>
2019-07-18 17:43:28 +0800

    修改用户登录访问后台服务

--- src/config/index.js
+++ src/config/index.js
@@ -17,7 +17,7 @@ export default {
    * @description api请求基础路径
    */
   baseUrl: {
-    dev: 'http://127.0.0.1:30000',
+    dev: 'http://127.0.0.1:20015',
     pro: 'https://produce.com'
   },
   /**
--- src/store/module/user.js
+++ src/store/module/user.js
@@ -82,8 +82,7 @@ export default {
           password
         }).then(res => {
           const data = res.data
-          console.log('data: ' + data)
-          commit('setToken', data.token)
+          commit('setToken', data)
           resolve()
         }).catch(err => {
           reject(err)
