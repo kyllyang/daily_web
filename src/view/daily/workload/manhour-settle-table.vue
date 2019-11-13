dacd86791242b61e027daf6051016f33184633e4
kyllyang <kyllyang@gmail.com>
2019-11-13 13:22:48 +0800

    增加工时汇总与结算方案导出功能

--- src/view/daily/workload/manhour-settle-table.vue
+++ src/view/daily/workload/manhour-settle-table.vue
@@ -7,17 +7,7 @@
         <Form ref="formData" :model="formData" :rules="formRule" :label-width="80" inline>
           <Row>
             <Col span="8">
-              <FormItem label="起始日期" prop="startWorkDate">
-                <DatePicker :value="formData.startWorkDate" format="yyyy-MM" type="month" @on-change="getStartWorkDateTime"></DatePicker>
-              </FormItem>
-            </Col>
-            <Col span="8">
-              <FormItem label="结束日期" prop="endWorkDate">
-                <DatePicker :value="formData.endWorkDate" format="yyyy-MM" type="month" @on-change="getEndWorkDateTime"></DatePicker>
-              </FormItem>
-            </Col>
-            <Col span="8">
-              <FormItem label="项目名称" prop="systemItemCode">
+              <FormItem label="项目名称" prop="systemItemCode" style="width: 100%;">
                 <Select v-model="formData.systemItemCode" filterable clearable>
                   <Option v-for="(item, index) in systemItemList" :value="item.code" :label="item.name" :key="index">
                     <span>{{ item.name }}</span>
@@ -26,9 +16,7 @@
                 </Select>
               </FormItem>
             </Col>
-          </Row>
-          <Row>
-            <Col span="8" offset="16">
+            <Col span="8" offset="8">
               <FormItem>
                 <Button type="primary" @click="handleExportExcel()">
                   <Icon type="md-download"/>
@@ -54,31 +42,14 @@ import { downloadProcess } from '@/libs/util'
 
 export default {
   data () {
-    const workDateValidator = (rule, value, callback) => {
-      if (value === '') {
-        callback(new Error((rule.field === 'startWorkDate' ? '起始' : '结束') + '日期不能为空'))
-      } else if (parseInt(this.formData.startWorkDate.replace('-', '')) > parseInt(this.formData.endWorkDate.replace('-', ''))) {
-        callback(new Error('起始日期不能大于结束日期'))
-      } else {
-        callback()
-      }
-    }
     return {
       collapse: '1',
       systemItemList: [],
       formData: {
-        startWorkDate: '',
-        endWorkDate: '',
         systemItemCode: '',
         fileName: '工时汇总与结算方案'
       },
       formRule: {
-        startWorkDate: [
-          { type: 'string', validator: workDateValidator, trigger: 'blur' }
-        ],
-        endWorkDate: [
-          { type: 'string', validator: workDateValidator, trigger: 'blur' }
-        ],
         systemItemCode: [
           { type: 'string', required: true, message: '不能为空', trigger: 'blur' }
         ]
@@ -89,9 +60,9 @@ export default {
     handleExportExcel () {
       this.$refs['formData'].validate((valid) => {
         if (valid) {
+          this.$Spin.show()
+
           exportExcelWorkloadManhourSettle({
-            startWorkDate: this.formData.startWorkDate,
-            endWorkDate: this.formData.endWorkDate,
             systemItemCode: this.formData.systemItemCode,
             fileName: this.formData.fileName,
             pageNo: this.pageNo,
@@ -100,6 +71,7 @@ export default {
             pageOrder: 'asc'
           }).then(res => {
             downloadProcess(res.data, this.formData.fileName + '.xlsx')
+            this.$Spin.hide()
           })
         } else {
           this.$Modal.warning({
@@ -111,12 +83,6 @@ export default {
     },
     handleReset () {
       this.$refs['formData'].resetFields()
-    },
-    getStartWorkDateTime (time) {
-      this.formData.startWorkDate = time
-    },
-    getEndWorkDateTime (time) {
-      this.formData.endWorkDate = time
     },
     loadSystemItemList () {
       listProjectSystemItem().then(res => {
